2b0f4cc1b19724e55a0d71526ecf1a282b43cfda
Chris <ghost@drednaut.net>
2023-01-11 08:03:52 -0800

    switched to postgres
    Signed-off-by: Chris <ghost@drednaut.net>

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import TwitterProvider from "next-auth/providers/twitter";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -24,6 +25,11 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
     // ...add more providers here
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+      version: "2.0", // opt-in to Twitter OAuth 2.0
+    }),
   ],
 };
 
